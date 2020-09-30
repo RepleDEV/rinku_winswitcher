3e37112fa0b963814a52308a1bdda2873dcef688
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2020-09-30 13:44:40 +0700

    Should NOW fix typescipt referencing issues

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,25 @@
-/// <reference types="node" />
-
-/**
- * Window Switcher class.
- */
-declare class WinSwitcher {
-    /**
-     * Class Constructor
-     */
-    constructor();
-    /**
-     * Stores the current active window.
-     */
-    storeActiveWindow(): String;
-    /**
-     * Activates the window that has been stored.
-     */
-    activateStoredWindow(): String;
-    /**
-     * Gets current active window's title.
-     */
-    static getActiveWindowTitle(): String;
-    /**
-     * Check the title of the stored window (if there's any).
-     */
-    checkStoredWindow(): String;
+declare module "rinku_winswitcher" {
+    class WinSwitcher {
+        /**
+         * Class Constructor
+         */
+        constructor();
+        /**
+         * Stores the current active window.
+         */
+        storeActiveWindow(): String;
+        /**
+         * Activates the window that has been stored.
+         */
+        activateStoredWindow(): String;
+        /**
+         * Gets current active window's title.
+         */
+        static getActiveWindowTitle(): String;
+        /**
+         * Check the title of the stored window (if there's any).
+         */
+        checkStoredWindow(): String;
+    }
+    export = WinSwitcher;
 }
-
-export default WinSwitcher;
