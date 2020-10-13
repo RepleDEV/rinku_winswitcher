3545ef87d444c2244ea4657e23a36d9ee54a34f2
RepleDEV <ragazzochaesa@gmail.com>
2020-10-13 09:56:30 +0700

    Forgot to update last compile time

--- build/Release/.deps/Release/rinku_winswitcher-linux.node.d
+++ build/Release/.deps/Release/rinku_winswitcher-linux.node.d
@@ -1,1 +1,1 @@
-cmd_Release/rinku_winswitcher-linux.node := ln -f "Release/obj.target/rinku_winswitcher-linux.node" "Release/rinku_winswitcher-linux.node" 2>/dev/null || (rm -rf "Release/rinku_winswitcher-linux.node" && cp -af "Release/obj.target/rinku_winswitcher-linux.node" "Release/rinku_winswitcher-linux.node")
+cmd_Release/rinku_winswitcher-linux.node := rm -rf "Release/rinku_winswitcher-linux.node" && cp -af "Release/obj.target/rinku_winswitcher-linux.node" "Release/rinku_winswitcher-linux.node"
